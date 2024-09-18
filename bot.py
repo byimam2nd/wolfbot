8e320b080c6ba92a9b26b670f23f379edca81ca4
byimam2nd <52017796+byimam2nd@users.noreply.github.com>
2024-09-18 04:21:44 +0000

    Perbaikan

--- bot.py
+++ bot.py
@@ -365,7 +365,7 @@ class playDice():
 
 #PROGRAM EKSEKUTOR -------------------->>      
 def eksekutor(data):
-  global timer, interAPI, playDice, 
+  global timer, interAPI, playDice, playGame, executor
   #Instance Class ( Initialize )
   #Instance Class timer
   timer = timer()
